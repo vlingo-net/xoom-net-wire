d6b8920d666f7c9a39570489b813a8ed6aafea9a
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-01-26 15:18:09 +0100

    Implement AddressType

---
+++ src/Vlingo.Wire/Node/AddressType.cs
@@ -0,0 +1,47 @@
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using System.Globalization;
+
+namespace Vlingo.Wire.Node
+{
+    public class AddressType : IComparable<AddressType>
+    {
+        public static readonly AddressType Main = new AddressType("addr=");
+        public static readonly AddressType Op = new AddressType("op=");
+        public static readonly AddressType App = new AddressType("app=");
+        public static readonly AddressType None = new AddressType("");
+
+        private AddressType(string field)
+        {
+            Field = field;
+        }
+        
+        public string Field { get; }
+        
+        public int CompareTo(AddressType other)
+        {
+            if (other == null || other.GetType() != typeof(AddressType))
+            {
+                return 1;
+            }
+            return String.Compare(Field, other.Field, StringComparison.InvariantCulture);
+        }
+        
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != typeof(AddressType))
+            {
+                return false;
+            }
+
+            return Field.Equals(((AddressType)obj).Field);
+        }
+
+        public override int GetHashCode() => 31 * Field.GetHashCode();
+    }
+}

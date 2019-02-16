dab55d28bc6a884df7342a584bd77a55dd48c47d
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-02-16 13:13:27 +0100

    Introducing System.IO.Pipelines to SocketChannelSelectionProcessorPipelineActor

--- src/Vlingo.Wire/Channel/IRequestChannelConsumer.cs
+++ src/Vlingo.Wire/Channel/IRequestChannelConsumer.cs
@@ -5,6 +5,7 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
+using System.Buffers;
 using Vlingo.Wire.Message;
 
 namespace Vlingo.Wire.Channel
@@ -13,5 +14,8 @@ namespace Vlingo.Wire.Channel
     {
         void CloseWith<T>(RequestResponseContext<T> requestResponseContext, object data);
         void Consume<T>(RequestResponseContext<T> context, IConsumerByteBuffer buffer);
+        
+        // TODO: Experimental System.IO.Pipelines
+        void Consume<T>(RequestResponseContext<T> context, ReadOnlySequence<byte> buffer);
     }
 }

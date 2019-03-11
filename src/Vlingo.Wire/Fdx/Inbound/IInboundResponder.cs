4ca40f1e6a47d0384a99d410fb46e08d2c266d7b
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-03-11 23:02:19 +0100

    Add IInboundResponder

---
+++ src/Vlingo.Wire/Fdx/Inbound/IInboundResponder.cs
@@ -0,0 +1,16 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System.IO;
+
+namespace Vlingo.Wire.Fdx.Inbound
+{
+    public interface IInboundResponder
+    {
+        void RespondWith(Stream buffer);
+    }
+}

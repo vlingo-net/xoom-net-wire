676beec627385b01aa6e1da676a01b9e1ddc2bc6
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-04-04 21:02:01 +0200

    [WIP] Managed Outbound Socket

---
+++ src/Vlingo.Wire.Tests/Fdx/Outbound/ManagedOutboundSocketChannelProviderTest.cs
@@ -0,0 +1,16 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Vlingo.Wire.Tests.Message;
+
+namespace Vlingo.Wire.Tests.Fdx.Outbound
+{
+    public class ManagedOutboundSocketChannelProviderTest : AbstractMessageTool
+    {
+        
+    }
+}

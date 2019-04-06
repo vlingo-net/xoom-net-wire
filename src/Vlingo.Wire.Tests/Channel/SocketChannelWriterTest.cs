8a491b97fb0d9dd4453e4c8a30ea16ec6698862a
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-04-06 23:51:35 +0200

    Implement missing SocketChannelWriterTest

--- src/Vlingo.Wire.Tests/Channel/SocketChannelWriterTest.cs
+++ src/Vlingo.Wire.Tests/Channel/SocketChannelWriterTest.cs
@@ -5,15 +5,88 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Vlingo.Actors.Plugin.Logging.Console;
+using Vlingo.Wire.Channel;
+using Vlingo.Wire.Fdx.Inbound;
+using Vlingo.Wire.Message;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Vlingo.Wire.Tests.Channel
 {
-    public class SocketChannelWriterTest
+    using Vlingo.Wire.Node;
+    
+    public class SocketChannelWriterTest : IDisposable
     {
+        private static readonly string TestMessage = "TEST ";
+
+        private SocketChannelWriter _channelWriter;
+        private IChannelReader _channelReader;
+        
         [Fact]
-        public void TestChannelWriter()
+        public async Task TestChannelWriter()
         {
+            var consumer = new MockChannelReaderConsumer();
+            
+            _channelReader.OpenFor(consumer);
+            
+            var buffer = new MemoryStream(1024);
+            buffer.SetLength(1024);
+            
+            var message1 = TestMessage + 1;
+            var rawMessage1 = RawMessage.From(0, 0, message1);
+            await _channelWriter.WriteAsync(rawMessage1, buffer);
+            
+            await ProbeUntilConsumedAsync(_channelReader, consumer);
+            
+            Assert.Equal(1, consumer.ConsumeCount);
+            Assert.Equal(message1, consumer.Messages.First());
+            
+            var message2 = TestMessage + 2;
+            var rawMessage2 = RawMessage.From(0, 0, message2);
+            await _channelWriter.WriteAsync(rawMessage2, buffer);
+            
+            await ProbeUntilConsumedAsync(_channelReader, consumer);
+            
+            Assert.Equal(2, consumer.ConsumeCount);
+            Assert.Equal(message2, consumer.Messages.Last());
+            
+        }
+
+        public SocketChannelWriterTest(ITestOutputHelper output)
+        {
+            var converter = new Converter(output);
+            Console.SetOut(converter);
+            
+            var node = Node.With(Id.Of(2), Name.Of("node2"), Host.Of("localhost"), 37373, 37374);
+            var logger = ConsoleLogger.TestInstance();
+            _channelWriter = new SocketChannelWriter(node.OperationalAddress, logger);
+            _channelReader = new SocketChannelInboundReader(node.OperationalAddress.Port, "test-reader", 1024, logger);
+        }
+
+        public void Dispose()
+        {
+            _channelWriter.Close();
+            _channelReader.Close();
+        }
+        
+        private async Task ProbeUntilConsumedAsync(IChannelReader reader, MockChannelReaderConsumer consumer)
+        {
+            var currentConsumedCount = consumer.ConsumeCount;
+    
+            for (int idx = 0; idx < 100; ++idx)
+            {
+                await reader.ProbeChannelAsync();
+
+                if (consumer.ConsumeCount > currentConsumedCount)
+                {
+                    break;
+                }
+            }
         }
     }
 }

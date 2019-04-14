a2e1c8803e4a19a8e7cc2c24bb92db5ad77ab975
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-04-14 20:14:03 +0200

    Fix #17. Implements IApplicationOutboundStream, ApplicationOutboundStreamActor and proxy. This was needed for the cluster implementation.

---
+++ src/Vlingo.Wire/Fdx/Outbound/ApplicationOutboundStream__Proxy.cs
@@ -0,0 +1,92 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using Vlingo.Actors;
+using Vlingo.Wire.Message;
+using Vlingo.Wire.Node;
+
+namespace Vlingo.Wire.Fdx.Outbound
+{
+    public class ApplicationOutboundStream__Proxy : IApplicationOutboundStream
+    {
+        private const string RepresentationStop1 = "Stop()";
+        private const string RepresentationBroadcast2 = "Broadcast(RawMessage)";
+        private const string RepresentationSendTo3 = "SendTo(RawMessage, Id)";
+
+        private readonly Actor _actor;
+        private readonly IMailbox _mailbox;
+
+        public ApplicationOutboundStream__Proxy(Actor actor, IMailbox mailbox)
+        {
+            _actor = actor;
+            _mailbox = mailbox;
+        }
+        
+        public void Stop()
+        {
+            if (!_actor.IsStopped)
+            {
+                Action<IApplicationOutboundStream> consumer = actor => actor.Stop();
+                if (_mailbox.IsPreallocated)
+                {
+                    _mailbox.Send(_actor, consumer, null, RepresentationStop1);
+                }
+                else
+                {
+                    _mailbox.Send(new LocalMessage<IApplicationOutboundStream>(_actor, consumer, RepresentationStop1));
+                }
+            }
+            else
+            {
+                _actor.DeadLetters.FailedDelivery(new DeadLetter(_actor, RepresentationStop1));
+            }
+        }
+
+        public bool IsStopped => _actor.IsStopped;
+        
+        public void Broadcast(RawMessage message)
+        {
+            if (!_actor.IsStopped)
+            {
+                Action<IApplicationOutboundStream> consumer = actor => actor.Broadcast(message);
+                if (_mailbox.IsPreallocated)
+                {
+                    _mailbox.Send(_actor, consumer, null, RepresentationBroadcast2);
+                }
+                else
+                {
+                    _mailbox.Send(new LocalMessage<IApplicationOutboundStream>(_actor, consumer, RepresentationBroadcast2));
+                }
+            }
+            else
+            {
+                _actor.DeadLetters.FailedDelivery(new DeadLetter(_actor, RepresentationBroadcast2));
+            }
+        }
+
+        public void SendTo(RawMessage message, Id targetId)
+        {
+            if (!_actor.IsStopped)
+            {
+                Action<IApplicationOutboundStream> consumer = actor => actor.SendTo(message, targetId);
+                if (_mailbox.IsPreallocated)
+                {
+                    _mailbox.Send(_actor, consumer, null, RepresentationSendTo3);
+                }
+                else
+                {
+                    _mailbox.Send(new LocalMessage<IApplicationOutboundStream>(_actor, consumer, RepresentationSendTo3));
+                }
+            }
+            else
+            {
+                _actor.DeadLetters.FailedDelivery(new DeadLetter(_actor, RepresentationSendTo3));
+            }
+        }
+    }
+}
---
+++ src/Vlingo.Wire/Fdx/Outbound/IApplicationOutboundStream.cs
@@ -0,0 +1,37 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Vlingo.Actors;
+using Vlingo.Wire.Message;
+using Vlingo.Wire.Node;
+
+namespace Vlingo.Wire.Fdx.Outbound
+{
+    public interface IApplicationOutboundStream : IStoppable
+    {
+        void Broadcast(RawMessage message);
+        
+        void SendTo(RawMessage message, Id targetId);
+    }
+
+    public static class ApplicationOutboundStreamFactory
+    {
+        public static IApplicationOutboundStream Instance(
+            Stage stage,
+            IManagedOutboundChannelProvider provider,
+            ByteBufferPool byteBufferPool)
+        {
+            var definition = Definition.Has<ApplicationOutboundStreamActor>(
+                Definition.Parameters(provider, byteBufferPool), "application-outbound-stream");
+            
+            var applicationOutboundStream =
+                stage.ActorFor<IApplicationOutboundStream>(definition);
+
+            return applicationOutboundStream;
+        }
+    }
+}

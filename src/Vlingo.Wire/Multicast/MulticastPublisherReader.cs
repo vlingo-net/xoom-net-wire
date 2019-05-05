c8b9618201042c22ad92834ee63bca49bb4e02d8
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-05-05 22:26:44 +0200

    MulticastPublisherReader should return "localhost" on name resolution for "::" loopback address

--- src/Vlingo.Wire/Multicast/MulticastPublisherReader.cs
+++ src/Vlingo.Wire/Multicast/MulticastPublisherReader.cs
@@ -193,6 +193,11 @@ namespace Vlingo.Wire.Multicast
             }
             catch
             {
+                if (publisherAddress.ToString().StartsWith("::"))
+                {
+                    return "localhost";
+                }
+                
                 return publisherAddress.ToString();
             }
         }

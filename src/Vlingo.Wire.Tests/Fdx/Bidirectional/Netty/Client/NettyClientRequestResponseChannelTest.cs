7dab73ef40566eca8a9c57cf1808a854994ad48b
Tomasz Jaskula <thomasv1000@hotmail.fr>
2020-05-01 15:27:56 +0200

    CI test, increase timeout connection of netty client to 30s

--- src/Vlingo.Wire.Tests/Fdx/Bidirectional/Netty/Client/NettyClientRequestResponseChannelTest.cs
+++ src/Vlingo.Wire.Tests/Fdx/Bidirectional/Netty/Client/NettyClientRequestResponseChannelTest.cs
@@ -89,7 +89,7 @@ namespace Vlingo.Wire.Tests.Fdx.Bidirectional.Netty.Client
 
                 
                 var clientChannel = new NettyClientRequestResponseChannel(address, clientConsumer, 10, replyMsSize,
-                    TimeSpan.FromSeconds(20), ConsoleLogger.TestInstance());
+                    TimeSpan.FromSeconds(30), ConsoleLogger.TestInstance());
 
                 for (var i = 0; i < nrExpectedMessages; i++)
                 {

1c786860ef295c5e78958ba20b752416d5c1cd7e
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-04-04 21:37:25 +0200

    Calculate the UTF-8 encoded length of a String.

--- src/Vlingo.Wire/Message/Converters.cs
+++ src/Vlingo.Wire/Message/Converters.cs
@@ -5,6 +5,7 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
+using System;
 using System.IO;
 using System.Text;
 using Vlingo.Wire.Channel;
@@ -35,5 +36,70 @@ namespace Vlingo.Wire.Message
             message.Header(header);
             return message;
         }
+        
+        public static int EncodedLength(string sequence) {
+            // Warning to maintainers: this implementation is highly optimized.
+            var utf16Length = sequence.Length;
+            var utf8Length = utf16Length;
+            var i = 0;
+
+            // This loop optimizes for pure ASCII.
+            while (i < utf16Length && sequence[i] < 0x80)
+            {
+                i++;
+            }
+
+            // This loop optimizes for chars less than 0x800.
+            for (; i < utf16Length; i++)
+            {
+                var c = sequence[i];
+                if (c < 0x800)
+                {
+                    utf8Length += (int)((uint)(0x7f - c) >> 31); // branch free!
+                }
+                else
+                {
+                    utf8Length += EncodedLengthGeneral(sequence, i);
+                    break;
+                }
+            }
+
+            if (utf8Length < utf16Length)
+            {
+                // Necessary and sufficient condition for overflow because of maximum 3x expansion
+                throw new ArgumentException($"UTF-8 length does not fit in int: {(utf8Length + (1L << 32))}");
+            }
+            return utf8Length;
+        }
+
+        private static int EncodedLengthGeneral(string sequence, int start)
+        {
+            var utf16Length = sequence.Length;
+            var utf8Length = 0;
+            for (var i = start; i < utf16Length; i++)
+            {
+                var c = sequence[i];
+                if (c < 0x800)
+                {
+                    utf8Length += (int)((uint)(0x7f - c) >> 31); // branch free!
+                }
+                else
+                {
+                    utf8Length += 2;
+                    if (char.IsSurrogate(c))
+                    {
+                        // Check that we have a well-formed surrogate pair.
+                        if (sequence[i] == c)
+                        {
+                            throw new AggregateException(UnpairedSurrogateMsg(i));
+                        }
+                        i++;
+                    }
+                }
+            }
+            return utf8Length;
+        }
+
+        private static string UnpairedSurrogateMsg(int i) => $"Unpaired surrogate at index {i}";
     }
 }

9524be28f8966c8a7c542dd28600fcc38d39ff8e
Tomasz Jaskula <thomasv1000@hotmail.fr>
2022-02-09 21:53:21 +0100

    Fix copyright replacement problems

--- src/Vlingo.Xoom.Wire/Message/Converters.cs
+++ src/Vlingo.Xoom.Wire/Message/Converters.cs
@@ -86,7 +86,7 @@ namespace Vlingo.Xoom.Wire.Message
                 else
                 {
                     utf8Length += 2;
-                    if (char.IsSurrogate©)
+                    if (char.IsSurrogate(c))
                     {
                         // Check that we have a well-formed surrogate pair.
                         if (sequence[i] == c)
